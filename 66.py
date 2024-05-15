c7a1e4a2607265c5c575fb0f5149212ff175151c
Ashu <ashunand8804@gmail.com>
2024-05-15 20:17:42 +0530

    problem 66

---
+++ 66.py
@@ -0,0 +1,28 @@
+a=[1,2,3]
+
+sum=0
+x=len(a)-1
+
+for i in a:
+    z=i*(10**x)
+    sum+=z
+    x-=1
+
+
+new_sum=sum+1
+print(sum)
+
+l1=[]
+a=0
+b=new_sum
+while new_sum>0:
+    a=new_sum%10
+    new_sum//=10
+
+    l1.append(a)
+
+l2=l1[::-1]
+
+print(l2)
+
+# array.map(), array.reduce()
