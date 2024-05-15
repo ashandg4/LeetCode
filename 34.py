13cd0826a0d552bef3ae28e7ca38a229c18513ad
Ashu <ashunand8804@gmail.com>
2024-05-15 20:21:39 +0530

    add another method

--- 34.py
+++ 34.py
@@ -1,7 +1,7 @@
 array1=[5,7,7,8,8,10]
 target=8
 
-print("Original array: ",array1)
+# print("Original array: ",array1)
 
 # for i in range(0,len(array1)):
 #     for j in range(i+1,len(array1)):
@@ -10,24 +10,25 @@ print("Original array: ",array1)
 # print(array1)
 
 
-# PREVIOUS SOLUTION (DON'T KNOW WHY IT'S WRONG)
-
-# l=[]
-# if target in array1:
-#     i=0
-#     for j in array1:
-#         if j==target:
-#             l.append(i)
-#         i+=1       
-# elif target not in array1:
-#     l.extend([-1,-1])
-# print(l)
-
-
-
+# Method-1
+l=[]
+if target in array1:
+    i=0
+    for j in array1:
+        if j==target:
+            l.append(i)
+        i+=1  
+    if len(l)==1:
+        l.append(l[0])
+elif target not in array1:
+    l.extend([-1,-1])
+print([l[0],l[-1]])
 
 
 
+
+
+# Method-2
 length=len(array1)-1
 if target in array1:
     array2=array1[::-1]
