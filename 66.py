8a6cc7e0598f47a9b26a530e9ac642016d714a49
Ashu <ashunand8804@gmail.com>
2024-05-17 08:40:09 +0530

    change few variables

--- 66.py
+++ 66.py
@@ -1,28 +1,26 @@
-a=[1,2,3]
+a = [1, 2, 3]
 
-sum=0
-x=len(a)-1
+sum = 0
+x = len(a)-1
 
 for i in a:
-    z=i*(10**x)
-    sum+=z
-    x-=1
+    z = i*(10**x)
+    sum += z
+    x -= 1
 
 
-new_sum=sum+1
+new_sum = sum+1
 print(sum)
 
-l1=[]
-a=0
-b=new_sum
-while new_sum>0:
-    a=new_sum%10
-    new_sum//=10
+l1 = []
+a = 0
+b = new_sum
+while new_sum > 0:
+    a = new_sum % 10
+    new_sum //= 10
 
     l1.append(a)
 
-l2=l1[::-1]
+l2 = l1[::-1]
 
 print(l2)
-
-# array.map(), array.reduce()
