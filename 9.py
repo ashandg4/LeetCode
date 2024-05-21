bd2f98ba988349a10b84b0a61d90541f18388dd4
Ashu <ashunand8804@gmail.com>
2024-05-21 23:41:48 +0530

    add some comments

--- 9.py
+++ 9.py
@@ -1,11 +1,13 @@
-x=int(input("Enter a number: "))
+# Palindrome Number
 
-if x<0:
+x = int(input("Enter a number: "))
+
+if x < 0:
     print(False)
 else:
-    a=0
-    b=x
-    while x>0:
-        a=a*10 + x%10
-        x//=10
-    print(a==b)
+    a = 0
+    b = x
+    while x > 0:
+        a = a*10 + x % 10
+        x //= 10
+    print(a == b)
