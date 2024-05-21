605e64ec1dfb2ff7f7ae61934c2e8329594feac1
Ashu <ashunand8804@gmail.com>
2024-05-21 09:33:59 +0530

    problem 58

---
+++ 58.py
@@ -0,0 +1,14 @@
+s = "ashu anand   "
+length = len(s) - 1
+
+while length >= 0 and s[length] == ' ':
+    length -= 1
+
+# now length points to the last character of the last word
+j = length
+
+while j >= 0 and s[j] != ' ':       # Find the space before the start of the last word
+    j -= 1
+
+# The length of the last word is the difference between i and j
+print(length-j)
