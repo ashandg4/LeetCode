f79ad689098c899cf3fa61a57a49561fb4287075
Ashu <ashunand8804@gmail.com>
2024-05-21 18:13:13 +0530

    problem 125

---
+++ 125.py
@@ -0,0 +1,19 @@
+def pal(s):
+    l = 0
+    r = len(s) - 1
+
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while l < r and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1
+        r -= 1
+
+    return True
+
+
+s = "A man, a plan, a cana: Panama"
+print(pal(s))
