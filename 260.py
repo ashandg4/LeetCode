6e5fbb5880208ef8e84f51949a7ede4dc1580d4a
Ashu <ashunand8804@gmail.com>
2024-05-31 12:06:35 +0530

    add more methods

--- 260.py
+++ 260.py
@@ -5,16 +5,28 @@ list2 = []
 
 # Method-1
 
-for i in nums:
-    if i not in list1:
-        list1.append(i)
-    else:
-        list2.append(i)
-result = [x for x in list1 if x not in list2]
-print(result)
-
 
 # Method-2
+s = set()
+for i in nums:
+    if i in s:
+        s.remove(i)
+    else:
+        s.add(i)
+print(list(s))
+
+
+# Method-3
+
+# for i in nums:
+#     if i not in list1:
+#         list1.append(i)
+#     else:
+#         list1.remove(i)
+# print(list2)
+
+
+# Method-4
 
 # for i in nums:
 #     if nums.count(i)==1:
